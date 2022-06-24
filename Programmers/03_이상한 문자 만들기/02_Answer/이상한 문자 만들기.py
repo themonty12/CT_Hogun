cced85abaf2ea7e56bab644c918cfbd1554b7d7a
SJH <sjh@hogun.co.kr>
2022-06-24 17:09:30 +0900

    [UPDATE] 이상한 문자 만들기

--- Programmers/03_이상한 문자 만들기/02_Answer/이상한 문자 만들기.py
+++ Programmers/03_이상한 문자 만들기/02_Answer/이상한 문자 만들기.py
@@ -1,8 +1,9 @@
 s = "abc abc abc "
 
 answer = ''
-
-for txt in s.split():
+#입력받은 문자열을 공백으로 split
+for txt in s.split(" "):
+    #나뉘어진 문자열의 홀수번째는 소문자로, 짝수번째는 대문자로 변경
     for i in range(len(txt)):
         if (i % 2) == 1:
            answer += txt[i].lower()
