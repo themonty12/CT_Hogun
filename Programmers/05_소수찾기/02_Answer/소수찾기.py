1a60ebff261959032e974da23d4769998a9d3061
SJH <sjh@hogun.co.kr>
2022-06-27 17:31:39 +0900

    [ADD] 소수찾기

---
+++ Programmers/05_소수찾기/02_Answer/소수찾기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    count = 0 
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            if i % j == 0:
+                count += 1            
+            if count > 2:
+                break
+        if count == 2:
+            answer += 1
+        count = 0
+    return answer
