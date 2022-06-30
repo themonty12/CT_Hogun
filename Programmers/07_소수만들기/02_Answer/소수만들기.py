8c9b39a94abc2797d1e55e4edc8eb5b4227d52fb
SJH <sjh@hogun.co.kr>
2022-06-30 17:38:09 +0900

    [UPDATE]소수 만들기

--- Programmers/07_소수만들기/02_Answer/소수만들기.py
+++ Programmers/07_소수만들기/02_Answer/소수만들기.py
@@ -3,10 +3,16 @@ def test(n):
         if n % i == 0:
             return False 
         return True
-
 temp = 0
 answer = -1
 li = [1,2,3,5,7,4]
 
- a = [i for i in li if i % 2 == 0]
- b = [i for i in li if i % 2 != 0]
+#a = [i for i in li if i % 2 == 0]
+#b = [i for i in li if i % 2 != 0]
+
+for i in range(len(li)):    
+    #for j in li(a+1,len(li)+1):
+    for j in range(i + 1, len(li)):
+        #print("i = " + li[i] + ", j = " + li[j])
+        print("i = {}, j = {}".format(li[i], li[j])
+        #print("i = {0}, j = {1}".format(li[i], li[j])
