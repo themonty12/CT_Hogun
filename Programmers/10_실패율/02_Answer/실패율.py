6489d25b2c68b0599746c38fe2afbb2055aa94c8
SJH <sjh@hogun.co.kr>
2022-07-12 17:43:26 +0900

    [UPDATE]실패율

--- Programmers/10_실패율/02_Answer/실패율.py
+++ Programmers/10_실패율/02_Answer/실패율.py
@@ -1,18 +1,27 @@
 N = 5
 stages = [2,1,2,6,2,4,3,3]
-li = []
+
 per = []
-answer = []
-#각 스테이지 머문 사람 수 찾기
-for i in range(N+1):
-    li.append(stages.count(i+1))
-print(li)
+sort = {}
+
 #머문 사람 수 / 총 스테이지 수 
-for j in range(0, len(li)):
-    per.append(li[j] / len(stages))
-    if not li[j] == 0:
-        for itm in stages:
-            if itm == (j+1):
-                stages.remove(itm)
-    print(stages)
-print(per)
+for j in range(1, N+1):
+    print("stages item : {}".format(len(stages)))
+    print("li[j] : {}".format(stages.count(j)))
+    per.append(stages.count(j)/ len(stages))
+    if not stages.count(j) == 0:
+        remove_set = [j]
+        stages = [itm for itm in stages if itm not in remove_set]
+        print(stages)
+            
+               
+for i in range(0, len(per)):
+    sort[i+1] = per[i]
+    
+print(sort)
+
+temp = sorted(sort.items(), key=lambda x:x[1], reverse = True)
+print(temp)
+for itm in range(0, len(temp)):
+    answer = temp[itm]
+print(answer)
