b55e5493ef96f7cc4ce0f15a84c57c27a847798d
SJH <sjh@hogun.co.kr>
2022-07-15 17:06:16 +0900

    [UPDATE]3진법뒤집기

--- Programmers/12_3진법 뒤집기/02_Answer/3진법 뒤집기.py
+++ Programmers/12_3진법 뒤집기/02_Answer/3진법 뒤집기.py
@@ -1,19 +1,16 @@
-n=45
-
+n=9
+answer = 0
 first_n = []
 #3으로 나뉘지 않을 때 까지 나누기
-while n > 3:
+while n >= 3:
     first_n.append(int(n%3))
     n=n/3
 #나머지는 리스트에 저장
 first_n.append(int(n))
-semi_ans = ""
-answer = 0
-#리스트 각 문자를 문자열로 변경(삭제)
-for i in first_n:
-    semi_ans += str(i)
+
 #문자열에 식 삽입
-for i in range(len(semi_ans)):
-    answer +=((3 ** (len(semi_ans)- i-1) *int(semi_ans[i])))
+for i in range(len(first_n)):
+
+    answer +=((3 ** (len(first_n)- i-1) *first_n[i]))
 print(answer)
     
