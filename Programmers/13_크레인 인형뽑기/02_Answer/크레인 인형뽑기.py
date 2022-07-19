eb04284eb89fe02d92e8cb4cb7fdf0c74bc6b110
SJH <sjh@hogun.co.kr>
2022-07-19 17:44:08 +0900

    [UPDATE]크레인 인형뽑기

--- Programmers/13_크레인 인형뽑기/02_Answer/크레인 인형뽑기.py
+++ Programmers/13_크레인 인형뽑기/02_Answer/크레인 인형뽑기.py
@@ -4,35 +4,31 @@ moves = [1,5,3,5,1,2,1,4]
 result = []
 # moves 값따라서 board 내용 확인
 for x in moves:
-    for i in range(len(board[x-1])-1, -1,-1):  
-        if not board[x-1][i] == 0:
-            result.append(board[x-1][i])
-            board[x-1][i] = 0
-
+    for i in range(0,len(board)):  
+        if board[i][x-1] == 0:
+            continue
+        else:
+            result.append(board[i][x-1])
+            board[i][x-1] = 0
             break
+print(result)
+
 #중복되면 answer에 사라진 숫자 개수만큼 더하기
 switch = True       
 while switch == True:
+    if len(result) < 2:
+        switch = False
+        continue
     for i in range(len(result)-1,0,-1):
-
         if result[i] == result[i-1]:     
             answer += 2
-            result.remove(i)
-            result.remove(i)
+            result.pop(i)
+            result.pop(i-1)
             switch = True
-            
             break
         else:
             switch = False
             
-    if len(result) < 2:
-        switch = False
+
 
 print(answer)
-    
-            
-    
-       
-        
-    
-    
