645370f0adcfe52184f10f00febe431713ae7719
SJH <sjh@hogun.co.kr>
2022-07-28 17:40:27 +0900

    [UPDATE]키패드 누르기

--- Programmers/16_키패드 누르기/02_Answer/키패드 누르기.py
+++ Programmers/16_키패드 누르기/02_Answer/키패드 누르기.py
@@ -1,8 +1,21 @@
-number = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
-hand = "right"
+number = [0,5,2,4,4,6,4,5,5,5,5,5]	
+hand = "left"
 result = ""
 rh = 12
 lh = 10
+
+def find_length(x,y):
+    if abs(x-y) == 1 or abs(x-y) == 3:
+        return 1
+    elif abs(x-y) == 2 or abs(x-y) == 4 or abs(x-y) == 6:
+        return 2
+    elif abs(x-y) == 5 or abs(x-y) == 7 or abs(x-y) == 9:
+        return 3
+    elif abs(x-y) == 0:
+        return 0
+    else:
+        return 4
+    
 #리스트 돌면서 왼손 오른손 판별
 for i in number:
     if i == 1 or i == 4 or i == 7:
@@ -12,33 +25,26 @@ for i in number:
         result += "R"
         rh = i
     else:
-        if abs(i - rh) > abs(i - lh):
-            result += "L"
-            lh = i
-        elif abs(i - rh) < abs(i - lh):
-            result += "R"
-            rh = i
+        
         #가운데 숫자일때 가까운 손 비교
+        if i == 0 :
+              i = 11
+
+        if find_length(i, lh) > find_length(i, rh):
+            rh = i
+            result += "R"
+        elif find_length(i, lh) < find_length(i, rh):
+            lh = i
+            result += "L"
         else:
-            if i == 0 :
-                i = 11
-            if find_length(i, lh) > find_length(i, rh):
-                result += "R"
-            elif find_length(i, lh) < find_length(i, rh):
-                result += "L"
-            else:
-                result += hand[0].upper()
+           result += hand[0].upper()
+           if hand[0].upper() == "L":
+                   lh = i
+           else:
+                   rh = i                      
 
 
-return result
+print( result)
 
 #가까운 손 비교 함
-def find_length(x,y):
-    if abs(x-y) == 1 or abs(x-y) == 3:
-        return 1
-    elif abs(x-y) == 2 or abs(x-y) == 4 or abs(x-y) == 6:
-        return 2
-    elif abs(x-y) == 5 or abs(x-y) == 7:
-        return 3
-    else:
-        return 4
+
